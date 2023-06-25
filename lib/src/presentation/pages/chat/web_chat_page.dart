67127b157c61d0200be1262152317e1046f7f8a3
riyanurdiansyah <riyanurdiansyah@gmail.com>
2023-06-25 12:22:29 +0700

    update

--- lib/src/presentation/pages/chat/web_chat_page.dart
+++ lib/src/presentation/pages/chat/web_chat_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -18,12 +17,12 @@ class _WebChatPageState extends State<WebChatPage> {
   @override
   void initState() {
     if (widget.no.isNotEmpty && widget.no[0] == "6" && widget.no[1] == "2") {
-      if (kIsWeb) {
-        launchUrl(Uri.parse(
-            "https://web.whatsapp.com/send/?phone=${widget.no}&text&type=phone_number&app_absent=0"));
-      } else {
-        launchUrl(Uri.parse("https://wa.me/${widget.no}"));
-      }
+      // if (kIsWeb) {
+      //   launchUrl(Uri.parse(
+      //       "https://web.whatsapp.com/send/?phone=${widget.no}&text&type=phone_number&app_absent=0"));
+      // } else {
+      launchUrl(Uri.parse("https://wa.me/${widget.no}"));
+      // }
     }
     super.initState();
   }
